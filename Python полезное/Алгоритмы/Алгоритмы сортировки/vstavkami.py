1ec1d433769053ca8c4a65737fc5405a8583dee5
max31ru12 <maxevg72@gmail.com>
2023-09-13 22:38:52 +0300

    Added info about django

--- Python полезное/Алгоритмы/Алгоритмы сортировки/vstavkami.py
+++ Python полезное/Алгоритмы/Алгоритмы сортировки/vstavkami.py
@@ -1,33 +1,33 @@
-from random import randint
 import time
 
-massive = [randint(0, 100) for i in range(10)]
 
-
-def decorator_function(func):
-
+def decor(function):
     def wrapper(*args, **kwargs):
 
         start = time.time()
-        func(*list(args))
+        res = function(*args)
+
         end = time.time() - start
         print(end)
+
+        return res
 
     return wrapper
 
 
-@decorator_function
-def sorting(mas):
+@decor
+def vstavka(massive: list):
 
-    for i in range(1, len(mas)):
+    for i in range(1, len(massive)):
 
         while i > 0:
-            if mas[i] < mas[i - 1]:
-                mas[i], mas[i - 1] = mas[i - 1], mas[i]
+
+            if massive[i] < massive[i - 1]:
+                massive[i], massive[i - 1] = massive[i - 1], massive[i]
+
             i -= 1
 
-    return print(mas)
+    return massive
 
 
-sorting(massive)
-
+print(vstavka([4, 3, 2, 1]))
