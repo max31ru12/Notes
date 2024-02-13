1a42bc682454140f51bae3a09715723111d1d05d
max31ru12 <maxevg72@gmail.com>
2024-02-13 19:25:37 +0300

    commt

--- Python/Асинхронное программирование/aiohttp_learn/main.py
+++ Python/Асинхронное программирование/aiohttp_learn/main.py
@@ -14,4 +14,15 @@ async def main():
         print(f"{status=}")
 
 
-asyncio.run(main())
+async def main_gather():
+    timeout = aiohttp.ClientTimeout(total=10, connect=0.1)
+    url = "https://www.example.com"
+    urls = [url for i in range(1000)]
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        requests = [session.get(l_url) for l_url in urls]
+        status_codes = await asyncio.gather(*requests, return_exceptions=True)
+        print(status_codes)
+        print(len(status_codes))
+
+
+asyncio.run(main_gather())
